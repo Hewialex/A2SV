f95334de777039c4a51653f243ce08d0e69c3b30
Hewialex <123300353+Hewialex@users.noreply.github.com>
2023-12-05 02:31:55 -0700

    Added solution for Convert the Temperature

---
+++ week1/convert-the-temperature.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.80 + 32.00
+        result = [round(kelvin, 5), round(fahrenheit, 5)]
+        return result
+        
