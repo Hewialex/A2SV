02df3ca9da6c72ac5edaba2352d27de4e582cccb
Hewialex <123300353+Hewialex@users.noreply.github.com>
2024-02-04 13:21:00 -0700

    Added solution for Fruit Into Baskets

--- week1/fruit-into-baskets.py
+++ week1/fruit-into-baskets.py
@@ -1,21 +1,20 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        ans = 0
-        diction = defaultdict(int)
-        l = 0
-        
-        for r in range(len(fruits)):
-            diction[fruits[r]] += 1
+        max_fruits = 0
+        left = 0
+        right = 0
+        counts = {}
 
-            while len(diction) > 2 :
-                diction[fruits[l]] -= 1
-                if diction[fruits[l]] == 0:
-                    del diction[fruits[l]]
-                l += 1
+        while right < len(fruits):
+            counts[fruits[right]] = counts.get(fruits[right], 0) + 1
+            right += 1
 
-            ans = max(r-l+1,ans)
-        return ans
+            while len(counts) > 2:
+                counts[fruits[left]] -= 1
+                if counts[fruits[left]] == 0:
+                    del counts[fruits[left]]
+                left += 1
 
+            max_fruits = max(max_fruits, right - left)
 
-
-        
+        return max_fruits
